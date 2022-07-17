04ade141df311643ca9e41507b64354fc766cded
riship <riship@nvidia.com>
2022-07-17 14:01:57 -0700

    working on it

--- custom_rgcnconv.py
+++ custom_rgcnconv.py
@@ -107,8 +107,7 @@ class RGCNConv(MessagePassing):
         else:
             self.register_parameter('bias', None)
 
-    def forward(self, x: Union[OptTensor, Tuple[OptTensor, Tensor]],
-                edge_index: Adj, edge_type: OptTensor = None, ptr = None):
+    def forward(self, x, edge_index, edge_type, ptr = None):
         r"""
         Args:
             x: The input node features. Can be either a :obj:`[num_nodes,
