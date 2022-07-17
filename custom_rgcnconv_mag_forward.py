0b97879221a7a827ad9f3ab7d9d76aa19bec82e6
riship <riship@nvidia.com>
2022-07-17 14:50:16 -0700

    working on it

--- custom_rgcnconv_mag_forward.py
+++ custom_rgcnconv_mag_forward.py
@@ -696,7 +696,7 @@ class Net(torch.nn.Module):
         x = F.relu(self.conv1(x, edge_index, edge_type, ptr))
         x = self.conv2(x, edge_index, edge_type, ptr)
         return F.log_softmax(x, dim=1)
-model = Net(bool(sys.arv[2])).to(sys.argv[1])
+model = Net(bool(sys.argv[2])).to(sys.argv[1])
 import time
 sumtime = 0
 
