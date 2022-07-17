fd9233ddc545b7643b43181fd90e03650b6637ab
riship <riship@nvidia.com>
2022-07-17 11:34:32 -0700

    working on it

--- custom_rgcnconv_mag_forward.py
+++ custom_rgcnconv_mag_forward.py
@@ -766,7 +766,7 @@ def fuse_batch(batch):
             src_type, dst_type = e_type[0], e_type[-1]
             e_idx_dict[e_type][0, :] = e_idx_dict[e_type][0, :] + increment_dict[src_type]
             e_idx_dict[e_type][1, :] = e_idx_dict[e_type][0, :] + increment_dict[dst_type]
-            etypes_list.append(torch.ones_like(e_idx_dict[e_type]) * i)
+            etypes_list.append(torch.ones(e_idx_dict[e_type].shape[-1]) * i)
     edge_types = torch.cat(etypes_list, dim=1)
     return x, torch.cat(list(e_idx_dict.values()), dim=1), edge_types
 for i, batch in enumerate(data_object.train_dataloader):
