f8fc508c7fe63789d27083fcdf0032d59e7a2288
riship <riship@nvidia.com>
2022-07-17 23:49:13 -0700

    working on it

--- custom_rgcnconv_mag_forward.py
+++ custom_rgcnconv_mag_forward.py
@@ -731,6 +731,9 @@ def fuse_batch(batch):
         if torch.numel(e_idx_dict[e_type]) != 0:
             e_idx_dict[e_type][0, :] = e_idx_dict[e_type][0, :] + increment_dict[src_type]
             e_idx_dict[e_type][1, :] = e_idx_dict[e_type][1, :] + increment_dict[dst_type]
+            # sort by dst nodes in ascending order
+            e_idx_dict[e_type][1, :], sort_indices = torch.sort(e_idx_dict[e_type][1, :])
+            e_idx_dict[e_type][0, :] = [0, sort_indices]
             etypes_list.append(torch.ones(e_idx_dict[e_type].shape[-1]) * i)
     edge_types = torch.cat(etypes_list)
     eidx = torch.cat(list(e_idx_dict.values()), dim=1)
