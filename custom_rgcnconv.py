4984cd38d787f5f466223e108a8bfb4a33d60ac6
riship <riship@nvidia.com>
2022-07-18 21:55:37 -0700

    working on it

--- custom_rgcnconv.py
+++ custom_rgcnconv.py
@@ -160,15 +160,15 @@ class RGCNConv(MessagePassing):
                 h.append(self.propagate(masked_edge_index(edge_index, edge_type == i), x=x_l, size=size) )     
             ptr = torch.tensor([i for i in range(0, h[0].shape[0] * (self.num_relations + 1), h[0].shape[0])])
             h = torch.cat(h)
-            print('inputs.shape=', h.shape)
-            print('ptr=',ptr)
-            print('weight.shape=', weight.shape)
-            assert not torch.isnan(h).any() and not torch.isinf(h).any()
-            assert not torch.isnan(weight).any() and not torch.isinf(weight).any()
+            # print('inputs.shape=', h.shape)
+            # print('ptr=',ptr)
+            # print('weight.shape=', weight.shape)
+            # assert not torch.isnan(h).any() and not torch.isinf(h).any()
+            # assert not torch.isnan(weight).any() and not torch.isinf(weight).any()
             o_tmp = torch.ops.pyg.segment_matmul(h, ptr, weight)
-            assert not torch.isnan(o_tmp).any() and not torch.isinf(o_tmp).any()
+            # assert not torch.isnan(o_tmp).any() and not torch.isinf(o_tmp).any()
             out += sum(torch.tensor_split(o_tmp, self.num_relations))
-            assert not torch.isnan(out).any() and not torch.isinf(out).any()
+            # assert not torch.isnan(out).any() and not torch.isinf(out).any()
             # h = self.propagate(edge_index, x=x_l, size=size)      
             # ptr = torch.tensor([i for i in range(0, h.shape[0] * (self.num_relations + 1), h.shape[0])])
             # h = h.repeat(self.num_relations, 1)
@@ -179,7 +179,7 @@ class RGCNConv(MessagePassing):
 
         if self.bias is not None:
             out += self.bias
-        assert not torch.isnan(out).any() and not torch.isinf(out).any()
+        # assert not torch.isnan(out).any() and not torch.isinf(out).any()
         # print('out.shape=',out.shape)
         return out
 
