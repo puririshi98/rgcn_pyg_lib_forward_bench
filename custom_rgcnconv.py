805bcf0d01a2ae51908683e391de71131bc78b0c
riship <riship@nvidia.com>
2022-07-19 01:45:02 -0700

    working on it

--- custom_rgcnconv.py
+++ custom_rgcnconv.py
@@ -181,11 +181,12 @@ class RGCNConv(MessagePassing):
             # out = sum(torch.tensor_split(torch.ops.pyg.segment_matmul(h, ptr, weight), self.num_relations))
 
             #attempt at reconciling the two (numerically correct and no for loops)
-            x_l = x_l.repeat(self.num_relations, 1)
+            
             ptr = torch.tensor([i for i in range(0, x_l.shape[0] * (self.num_relations + 1), x_l.shape[0])])
-            print('inputs.shape=', x_l.shape)
-            print('ptr=',ptr)
-            print('weight.shape=', weight.shape)
+            x_l = x_l.repeat(self.num_relations, 1)
+            # print('inputs.shape=', x_l.shape)
+            # print('ptr=',ptr)
+            # print('weight.shape=', weight.shape)
             x_in = sum(torch.tensor_split(torch.ops.pyg.segment_matmul(x_l, ptr, weight), self.num_relations))
             h = self.propagate(edge_index, x=x_in, size=size)
 
