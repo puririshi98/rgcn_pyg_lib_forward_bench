12f0cd13bab479bb0c2228981e33c7fe81491ae8
riship <riship@nvidia.com>
2022-07-21 13:07:20 -0700

    perf #s

--- custom_rgcnconv_2.py
+++ custom_rgcnconv_2.py
@@ -145,11 +145,10 @@ class RGCNConv(MessagePassing):
                 h = self.propagate(tmp, x=x_l, size=size)
                 out = out + (h @ weight[i])
         else:
-            edge_type, sort_by_edge_type = torch.sort(edge_type)
-            edge_index = edge_index[:, sort_by_edge_type]
-            self.edge_ptr = torch.cumsum(
-                torch.unique_consecutive(edge_type, return_counts=True)[1], dim=0)
-            self.edge_ptr = torch.cat((torch.tensor([0]), self.edge_ptr))
+            if (edge_type[1:] < edge_type[:-1]).any():
+                edge_type, sort_by_edge_type = torch.sort(edge_type)
+                edge_index = edge_index[:, sort_by_edge_type]
+            self.edge_ptr = torch.ops.torch_sparse.ind2ptr(edge_type, self.num_relations)
             out = self.propagate(edge_index, x=x_l, size=size)
 
 
