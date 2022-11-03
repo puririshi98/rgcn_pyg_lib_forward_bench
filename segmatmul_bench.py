8bd065d67992b186bd8e7d95662d7dcfa8890439
Rishi Puri <puririshi98@berkeley.edu>
2022-11-03 12:21:11 -0700

    Update segmatmul_bench.py

--- segmatmul_bench.py
+++ segmatmul_bench.py
@@ -13,10 +13,12 @@ for num_types in [4, 8, 16, 32, 64, 128, 256, 512, 1024]:
     inputs = torch.randn((num_nodes_per_type * num_types, n_feats)).cuda()
     ptr = torch.tensor(list(range(0, (num_types + 1) * num_nodes_per_type, num_nodes_per_type))).cuda()
     other = torch.randn((num_types, n_feats, out_feats), requires_grad=True).cuda()
-    since=time.time()
-    for i in range(10):
+    
+    for i in range(60):
+        if i==10:
+            since=time.time()
         pyg_lib.ops.segment_matmul(inputs, ptr, other)
-    times.append((time.time()-since)/10.0)
+    times.append((time.time()-since)/50.0)
     print("Avg time for", num_types, '=', times[-1])
 
 
