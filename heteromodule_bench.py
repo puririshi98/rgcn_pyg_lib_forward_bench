a870f6852e28c6c32a2d25a99e46125c0f7714fa
Rishi Puri <riship@riship-mlt.client.nvidia.com>
2022-11-18 12:33:40 -0800

    measure dict based heterolin

--- heteromodule_bench.py
+++ heteromodule_bench.py
@@ -14,21 +14,22 @@ out_feats = 64
 for num_types in [4, 8, 16, 32, 64, 128, 256, 512]:
     x_dict = {'v'+str(i):torch.randn((num_nodes_per_type, n_feats)).cuda() for i in range(num_types)}
     metadata= (x_dict.keys(), [])
-    heterolin = ToHeteroModule(Linear(n_feats, out_feats), metadata).cuda()
-    for i in range(60):
-        if i==10:
-            since=time.time()
-        heterolin(x_dict)
-    dict_times.append((time.time()-since)/50.0)
-    print("Avg time for", num_types, '=', times[-1])
     x = torch.cat(list(x_dict.values()), dim=0)
     node_type = torch.cat([(j * torch.ones(x_j.shape[0])).long()
                            for j, x_j in enumerate(x_dict.values())])
+    heterolin = ToHeteroModule(Linear(n_feats, out_feats), metadata).cuda()
     for i in range(60):
         if i==10:
             since=time.time()
         heterolin(x=x, node_type=node_type)
     fused_times.append((time.time()-since)/50.0)
+    print("Avg time for", num_types, '=', fused_times[-1])
+    for i in range(60):
+        if i==10:
+            since=time.time()
+        heterolin(x_dict)
+    dict_times.append((time.time()-since)/50.0)
+    print("Avg time for", num_types, '=', dict_times[-1])
 
 
 
