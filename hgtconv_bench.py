883df01d9b523f74b0922ca88ee9b21b84135c4d
Rishi Puri <puririshi98@berkeley.edu>
2023-01-09 13:44:35 -0800

    Create hgtconv_bench.py

---
+++ hgtconv_bench.py
@@ -0,0 +1,13 @@
+from torch_geometric.nn.conv import HGTConv
+from torch_geometric.datasets import FakeHeteroDataset
+import time
+data = FakeHeteroDataset().data
+net = HGTConv(-1, 32)
+x_dict = data.collect('x')
+edge_index_dict = data.collect('edge_index')
+for i in range(60):
+  if i > 9:
+    since = time.time()
+  net(edge_index_dict)
+print("average fwd pass time:", (time.time()-since)/50.0)
+  
