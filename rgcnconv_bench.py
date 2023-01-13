c4f68cd5688f840055d62de4ae1d0962f7dcc7ae
Rishi Puri <puririshi98@berkeley.edu>
2023-01-13 11:00:06 -0800

    Update rgcnconv_bench.py

--- rgcnconv_bench.py
+++ rgcnconv_bench.py
@@ -11,12 +11,11 @@ data[('n1','e3','n3'] = torch.randint(5000, size=(2,10000))
 data[('n2','e1_rev','n1'] = torch.randint(5000, size=(2,10000))
 data[('n3','e2_rev','n2'] = torch.randint(5000, size=(2,10000))
 data[('n3','e3_rev','n1'] = torch.randint(5000, size=(2,10000))
-      
+
 net = RGCNConv(in_channels=64, out_channels=32, num_relations=2)
-x_dict = data.collect('x')
-edge_index_dict = data.collect('edge_index')
+data = data.to('cuda').to_homogeneous()
 for i in range(60):
   if i > 9:
     since = time.time()
-  net(x_dict, edge_index_dict)
+  net(data.x, data.edge_index, data.edge_type)
 print("average fwd pass time:", (time.time()-since)/50.0)
