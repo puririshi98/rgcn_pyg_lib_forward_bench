1ef3231552413a82ba12eeaff6fda16a63f56dda
Rishi Puri <puririshi98@berkeley.edu>
2023-01-30 13:44:31 -0800

    Update hgtconv_scale_bench.py

--- hgtconv_scale_bench.py
+++ hgtconv_scale_bench.py
@@ -15,7 +15,7 @@ def bench(num_node_types=16, num_edge_types=128):
   return out_time
 
 fwd_times = {}
-for num_node_types in [2,4,8,16,32,64,128]:
-  for num_edge_types in [4, 8, 16, 32, 64, 256]:
+for num_node_types in [2,4,8,16,32,64]:
+  for num_edge_types in [4, 8, 16, 32, 64, 128, 256]:
     fwd_times[(num_node_types, num_edge_types)] = bench(num_node_types, num_edge_types)
     
