619e9ee32fca8d2f896650577cd95d824207ba51
Rishi Puri <puririshi98@berkeley.edu>
2023-03-30 12:11:02 -0700

    Update multi_aggr_bench.py

--- multi_aggr_bench.py
+++ multi_aggr_bench.py
@@ -1,7 +1,7 @@
 import torch
 import torch_geometric
 import time
-aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation(),torch_geometric.nn.aggr.MeanAggregation(),torch_geometric.nn.aggr.MinAggregation(),torch_geometric.nn.aggr.MaxAggregation()], mode="attn", mode_kwargs={'in_channels':64, 'out_channels':32, 'num_heads':512})
+aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation(),torch_geometric.nn.aggr.MeanAggregation(),torch_geometric.nn.aggr.MinAggregation(),torch_geometric.nn.aggr.MaxAggregation()], mode="attn", mode_kwargs={'in_channels':64, 'out_channels':32, 'num_heads':1024})
 x = torch.randn(50000,64)
 idx = torch.arange(50000)
 for i in range(60):
