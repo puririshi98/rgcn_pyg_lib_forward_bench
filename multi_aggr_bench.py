aa95d885ec79f121a995f494f4b6ba9e56b13954
Rishi Puri <puririshi98@berkeley.edu>
2023-03-30 14:27:03 -0700

    Update multi_aggr_bench.py

--- multi_aggr_bench.py
+++ multi_aggr_bench.py
@@ -1,9 +1,9 @@
 import torch
 import torch_geometric
 import time
-aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation(),torch_geometric.nn.aggr.MeanAggregation(),torch_geometric.nn.aggr.MinAggregation(),torch_geometric.nn.aggr.MaxAggregation()], mode="attn", mode_kwargs={'in_channels':2048, 'out_channels':1024, 'num_heads':128})
-x = torch.randn(50000,2048)
-idx = torch.arange(50000)
+aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation(),torch_geometric.nn.aggr.MeanAggregation(),torch_geometric.nn.aggr.MinAggregation(),torch_geometric.nn.aggr.MaxAggregation()], mode="attn", mode_kwargs={'in_channels':2048, 'out_channels':1024, 'num_heads':128}).cuda()
+x = torch.randn(50000,2048).cuda()
+idx = torch.arange(50000).cuda()
 for i in range(60):
   if i > 9:
     since=time.time()
