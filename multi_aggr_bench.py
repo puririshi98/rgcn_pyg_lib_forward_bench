0f71d38aaa7bc0a366c7b90670490d61f984a553
Rishi Puri <puririshi98@berkeley.edu>
2023-03-30 12:03:27 -0700

    Update multi_aggr_bench.py

--- multi_aggr_bench.py
+++ multi_aggr_bench.py
@@ -3,7 +3,7 @@ import torch_geometric
 import time
 aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation(),torch_geometric.nn.aggr.MeanAggregation(),torch_geometric.nn.aggr.MinAggregation(),torch_geometric.nn.aggr.MaxAggregation()], mode="attn", mode_kwargs={'in_channels':32, 'out_channels':16})
 x = torch.randn(50,32)
-idx = torch.randint(0,50,size=(2, 100))
+idx = torch.randint(0,50,size=(100))
 for i in range(60):
   if i > 9:
     since=time.time()
