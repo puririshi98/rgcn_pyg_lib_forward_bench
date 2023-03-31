c87518e45af953e0e2fd2f70ed8b6131b62c8181
Rishi Puri <puririshi98@berkeley.edu>
2023-03-31 12:37:04 -0700

    Update multi_aggr_bench.py

--- multi_aggr_bench.py
+++ multi_aggr_bench.py
@@ -1,6 +1,8 @@
 import torch
 import torch_geometric
 import time
+os.environ['NVIDIA_TF32_OVERRIDE'] = '0'
+
 aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation()]*128, mode="attn", mode_kwargs={'in_channels':128, 'out_channels':64, 'num_heads':8}).cuda()
 x = torch.randn(500,128).cuda()
 idx = torch.arange(500).cuda()
