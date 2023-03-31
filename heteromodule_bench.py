aaa533264462eb798fb01b2e7f0166d0e73761c8
Rishi Puri <puririshi98@berkeley.edu>
2023-03-31 16:30:46 -0700

    Update heteromodule_bench.py

--- heteromodule_bench.py
+++ heteromodule_bench.py
@@ -18,7 +18,7 @@ for num_types in [4, 8, 16, 32, 64, 128, 256, 512, 1024]:
     node_type = torch.cat([(j * torch.ones(x_j.shape[0])).long()
                            for j, x_j in enumerate(x_dict.values())])
     lin = Linear(n_feats, out_feats).cuda()
-    heterolin = ToHeteroLinear(lin,x_dict.keys())
+    heterolin = ToHeteroLinear(lin,x_dict.keys()).cuda()
     for i in range(60):
         if i==10:
             since=time.time()
