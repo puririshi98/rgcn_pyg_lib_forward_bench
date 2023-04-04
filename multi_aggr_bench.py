517455bd06ab5da4ef906b6854daeb40b1f1fe77
Rishi Puri <puririshi98@berkeley.edu>
2023-04-03 17:07:09 -0700

    Update multi_aggr_bench.py

--- multi_aggr_bench.py
+++ multi_aggr_bench.py
@@ -205,8 +205,8 @@ os.environ['NVIDIA_TF32_OVERRIDE'] = '0'
 aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation()]*256, mode="attn", mode_kwargs={'in_channels':128, 'out_channels':64, 'num_heads':8}).cuda()
 old_aggr = OldMultiAggregation([torch_geometric.nn.aggr.SumAggregation()]*256, mode="attn", mode_kwargs={'in_channels':128, 'out_channels':64, 'num_heads':8}).cuda()
 
-x = torch.randn(50000,128).cuda()
-idx = torch.arange(50000).cuda()
+x = torch.randn(5000,128).cuda()
+idx = torch.arange(5000).cuda()
 for i in range(60):
   if i > 9:
     since=time.time()
