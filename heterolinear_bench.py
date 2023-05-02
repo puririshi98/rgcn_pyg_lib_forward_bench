ff51aab0f4c602ebfe9e4bc7a46c340c3c79fed0
Rishi Puri <puririshi98@berkeley.edu>
2023-05-02 15:12:25 -0700

    Update heterolinear_bench.py

--- heterolinear_bench.py
+++ heterolinear_bench.py
@@ -15,7 +15,7 @@ for num_types in [4, 8, 16, 32, 64, 128, 256, 512, 1024]:
     x_dict = {'v'+str(i):torch.randn((num_nodes_per_type, n_feats)).cuda() for i in range(num_types)}
     x = torch.cat(list(x_dict.values()), dim=0)
     node_type = torch.cat([(j * torch.ones(x_j.shape[0])).long()
-                           for j, x_j in enumerate(x_dict.values())])
+                           for j, x_j in enumerate(x_dict.values())]).cuda()
     lin = Linear(n_feats, out_feats).cuda()
     heterolin = HeteroLinear(n_feats, out_feats, len(list(x_dict.keys())), True).cuda()
     for i in range(60):
@@ -27,8 +27,10 @@ for num_types in [4, 8, 16, 32, 64, 128, 256, 512, 1024]:
     for i in range(60):
         if i==10:
             since=time.time()
-        for i in range(num_types):
-            lin(x_dict['v'+str(i)])
+        o = x.new_empty(x.size(0), 64)
+        for j in range(num_types):
+            mask = j==node_type
+            o[mask] = lin(x[mask])
     loop_times.append((time.time()-since)/50.0)
     print("Avg time for for-loop", num_types, '=', loop_times[-1])
 
