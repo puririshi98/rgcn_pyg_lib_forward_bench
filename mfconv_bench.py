f170dc3f3393441d31c47f81af99c7ac6e6f80e0
Rishi Puri <puririshi98@berkeley.edu>
2023-05-15 13:29:04 -0700

    Update mfconv_bench.py

--- mfconv_bench.py
+++ mfconv_bench.py
@@ -12,7 +12,7 @@ try:
           max_dim = max(max_dim, data[n_type].x.size(-1))
         for n_type in data.node_types:
           if data[n_type].x.size(-1) < max_dim:
-            data[n_type].x = torch.cat((data[n_type].x, torch.zeros((data[n_type].x.size(0),max_dim - data[n_type].x.size(-1))), dim=-1)
+            data[n_type].x = torch.cat((data[n_type].x, torch.zeros((data[n_type].x.size(0),max_dim - data[n_type].x.size(-1)))), dim=-1)
         data = data.to_homogeneous().to('cuda')                           
         net = MFConv(in_channels=data.x.size(-1), out_channels=data.x.size(-1)/2, num_relations=len(data.edge_types)).cuda()
         for i in range(60):
