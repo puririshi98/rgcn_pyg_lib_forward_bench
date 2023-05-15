5545c99a305a65a917bd8c3bf039529487186aa7
Rishi Puri <puririshi98@berkeley.edu>
2023-05-15 13:40:26 -0700

    Update filmconv_bench.py

--- filmconv_bench.py
+++ filmconv_bench.py
@@ -1,8 +1,9 @@
-from torch_geometric.nn.conv import FiLMConv
+from torch_geometric.nn.conv import FastFiLMConv, FiLMConv
 from torch_geometric.datasets import FakeHeteroDataset
 import torch
 import time
 times = {}
+fast_times = {}
 try:
     for n_per_type in [100,1000,10000, 100000]:
         for n_types in [2,4,8,16,32,64,128,256]:
@@ -14,13 +15,22 @@ try:
             if data[n_type].x.size(-1) < max_dim:
               data[n_type].x = torch.cat((data[n_type].x, torch.zeros((data[n_type].x.size(0),max_dim - data[n_type].x.size(-1)))), dim=-1)
           data = data.to_homogeneous().to('cuda')                           
-          net = MFConv(in_channels=data.x.size(-1), out_channels=int(data.x.size(-1)/2), num_relations=n_types).cuda()
+          net = FiLMConv(in_channels=data.x.size(-1), out_channels=int(data.x.size(-1)/2), num_relations=n_types).cuda()
+          fastnet = FastFiLMConv(in_channels=data.x.size(-1), out_channels=int(data.x.size(-1)/2), num_relations=n_types).cuda()
           for i in range(60):
             if i > 9:
               since = time.time()
             net(data.x, data.edge_index, data.edge_type)
           times[(n_per_type, n_types)] = (time.time()-since)/50.0                               
-          print("average fwd pass time:", times[(n_per_type, n_types)])
+          print("average vanilla fwd pass time:", times[(n_per_type, n_types)])
+          for i in range(60):
+            if i > 9:
+              since = time.time()
+            fastnet(data.x, data.edge_index, data.edge_type)
+          fast_times[(n_per_type, n_types)] = (time.time()-since)/50.0                               
+          print("average fast fwd pass time:", fast_times[(n_per_type, n_types)])
 except:
     print("times=", times)
+    print("fast_times=", fast_times)
 print("times=", times)
+print("fast_times=", fast_times)
