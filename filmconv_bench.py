ff07b920763af026319f295751fefd246cc06d07
Rishi Puri <puririshi98@berkeley.edu>
2023-05-17 15:01:17 -0700

    Update filmconv_bench.py

--- filmconv_bench.py
+++ filmconv_bench.py
@@ -16,7 +16,7 @@ try:
               data[n_type].x = torch.cat((data[n_type].x, torch.zeros((data[n_type].x.size(0),max_dim - data[n_type].x.size(-1)))), dim=-1)
           data = data.to_homogeneous().to('cuda')                           
           net = FiLMConv(in_channels=data.x.size(-1), out_channels=int(data.x.size(-1)/2), num_relations=n_types).cuda()
-          fastnet = FastFiLMConv(in_channels=data.x.size(-1), out_channels=int(data.x.size(-1)/2), num_relations=n_types).cuda()
+          fastnet = FastFiLMConv(in_channels=data.x.size(-1), out_channels=int(data.x.size(-1)/2), num_relations=n_types, is_sorted=True).cuda()
           for i in range(60):
             if i > 9:
               since = time.time()
