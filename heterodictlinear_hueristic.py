3195f8c6058c28e2af3061eea1ac4a8cb178a3f8
Rishi Puri <puririshi98@berkeley.edu>
2023-05-24 10:29:45 -0700

    Create heterodictlinear_hueristic.py

---
+++ heterodictlinear_hueristic.py
@@ -0,0 +1,70 @@
+import torch
+from torch_geometric.nn.dense import Linear, HeteroLinear
+import time
+import os
+os.environ['NVIDIA_TF32_OVERRIDE'] = '0'
+fused_times = {}
+loop_times = {}
+try:
+  for num_nodes_per_type in [10**2,10**3,10**4,10**5]:
+    for out_feats in [2, 4,8,16,32,64,128,256]:
+      for n_feats in [4,8,16,32,64,128,256,512]:
+        for num_types in [4, 8, 16, 32, 64, 128, 256, 512]:
+          try:
+            if n_feats < out_feats:
+              continue
+            print("benchmarking", num_types,"types w/", num_nodes_per_type, "nodes per type and", n_feats, "input features and", out_feats, "outuput feats")
+            x_dict = {'v'+str(i):torch.randn((num_nodes_per_type, n_feats)).cuda() for i in range(num_types)}
+            x = torch.cat(list(x_dict.values()), dim=0)
+            node_type = torch.cat([(j * torch.ones(x_j.shape[0])).long()
+                                   for j, x_j in enumerate(x_dict.values())]).cuda()
+            lin = Linear(n_feats, out_feats).cuda()
+            heterolin = HeteroLinear(n_feats, out_feats, len(list(x_dict.keys())), True).cuda()
+            for i in range(60):
+                if i==10:
+                    since=time.time()
+                heterolin(x=x, type_vec=node_type)
+            key = (num_types, num_nodes_per_type, n_feats, out_feats)
+            fused_times[key] = ((time.time()-since)/50.0)
+            print("Avg time for fuse based=", fused_times[key])
+            for i in range(60):
+                if i==10:
+                    since=time.time()
+                o = x.new_empty(x.size(0), out_feats)
+                for j in range(num_types):
+                    mask = j==node_type
+                    o[mask] = lin(x[mask])
+            loop_times[key] = ((time.time()-since)/50.0)
+            print("Avg time for for-loop=", loop_times[key])
+          except:
+            continue
+except:
+  print("Loop Times:", loop_times)
+  print("Fused Times:", fused_times)
+
+
+
+
+print("Loop Times:", loop_times)
+print("Fused Times:", fused_times)
+
+print("done timing, now will learn hueristic w/ sklearn")
+import numpy as np
+X = np.zeros((len(loop_times), 4))
+y = np.zeros(len(loop_times))
+for i, key in enumerate(loop_times.keys()):
+	X[i, :] = key
+	loop_time, fused_time = loop_times[key], fused_times[key]
+	y[i] = int(fused_time <= loop_time)
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+scaler = StandardScaler()
+svm = LinearSVC()
+clf = make_pipeline(scaler, svm)
+clf.fit(X, y)
+
+print("scaler mean=", scaler.mean_)
+print("scaler scale=", scaler.scale_)
+print("svm weights=", svm.coef_)
+print("svm bias=", svm.intercept_)
