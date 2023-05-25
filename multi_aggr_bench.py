1aca6cf0c8cbc0214dcfb69bd86e1533ee9a7fab
Rishi Puri <puririshi98@berkeley.edu>
2023-05-25 14:35:40 -0700

    Update multi_aggr_bench.py

--- multi_aggr_bench.py
+++ multi_aggr_bench.py
@@ -202,8 +202,8 @@ class OldMultiAggregation(Aggregation):
         return f'{self.__class__.__name__}([\n{aggrs}], mode={self.mode})'
 os.environ['NVIDIA_TF32_OVERRIDE'] = '0'
 
-aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation()]*256, mode="attn", mode_kwargs={'in_channels':128, 'out_channels':64, 'num_heads':8}).cuda()
-old_aggr = OldMultiAggregation([torch_geometric.nn.aggr.SumAggregation()]*256, mode="attn", mode_kwargs={'in_channels':128, 'out_channels':64, 'num_heads':8}).cuda()
+aggr = torch_geometric.nn.aggr.MultiAggregation([torch_geometric.nn.aggr.SumAggregation()]*256, mode="attn", mode_kwargs={'in_channels':256, 'out_channels':64, 'num_heads':8}).cuda()
+old_aggr = OldMultiAggregation([torch_geometric.nn.aggr.SumAggregation()]*256, mode="attn", mode_kwargs={'in_channels':256, 'out_channels':64, 'num_heads':8}).cuda()
 
 x = torch.randn(10000,256).cuda()
 idx = torch.arange(10000).cuda()
