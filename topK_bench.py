80dbd1352fdcbab605f495c6ca7aee1e4dd22d24
Rishi Puri <puririshi98@berkeley.edu>
2023-06-07 10:56:22 -0700

    Update topK_bench.py

--- topK_bench.py
+++ topK_bench.py
@@ -6,7 +6,7 @@ try:
     for num_nodes in [100,1000,10000, 100000]:
         for num_feats in [2,4,8,16,32,64,128,256]:
           try:
-              x = torch.randn(num_nodes * num_feats, dtype=torch.float, device='cuda')
+              x = torch.randn((num_nodes * num_feats,), dtype=torch.float, device='cuda')
               batch = x.new_zeros(x.size(0), dtype=torch.long)
               for i in range(60):
                 if i > 9:
