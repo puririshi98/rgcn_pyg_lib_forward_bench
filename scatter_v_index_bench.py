5f33425d5f39ac10ea1ba70c4226c40c669c5dda
Rishi Puri <puririshi98@berkeley.edu>
2023-06-21 11:05:52 -0700

    Update scatter_v_index_bench.py

--- scatter_v_index_bench.py
+++ scatter_v_index_bench.py
@@ -5,17 +5,19 @@ def broadcast(src: Tensor, ref: Tensor, dim: int) -> Tensor:
     size = ((1, ) * dim) + (-1, ) + ((1, ) * (ref.dim() - dim - 1))
     return src.view(size).expand_as(ref)
 
+src = torch.randn(size=(100000,128)).to('cuda')
+index = torch.randint(high=src.size(0), size=(100000,)).to('cuda')
+dim_size = int(index.max()) + 1 if index.numel() > 0 else 0
+size = src.size()[:dim] + (dim_size, ) + src.size()[dim + 1:]
+dim = 0
+
 def og_func(index, src, dim):
   index = broadcast(index, src, dim)
-  size = src.size()[:dim] + (dim_size, ) + src.size()[dim + 1:]
   return src.new_zeros(size).scatter_add_(dim, index, src)
 
 def new_func(index, src, dim):
-  size = src.size()[:dim] + (dim_size, ) + src.size()[dim + 1:]
   return src.new_zeros(size).index_add_(dim, index, src)
-src = torch.randn(size=(100000,128)).to('cuda')
-index = torch.randint(high=src.size(0), size=(100000,)).to('cuda')
-dim = 0
+
 for i in range(60):
   if i > 9:
     since = time.time()
