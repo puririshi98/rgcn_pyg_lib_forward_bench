faf81b4b39bd515904ceb34e281df8ab15d20de2
Rishi Puri <puririshi98@berkeley.edu>
2023-06-21 11:02:24 -0700

    Update scatter_v_index_bench.py

--- scatter_v_index_bench.py
+++ scatter_v_index_bench.py
@@ -10,6 +10,7 @@ def og_func(index, src, dim):
   return src.new_zeros(size).scatter_add_(dim, index, src)
 
 def new_func(index, src, dim):
+  size = ((1, ) * dim) + (-1, ) + ((1, ) * (ref.dim() - dim - 1))
   return src.new_zeros(size).index_add_(dim, index, src)
 src = torch.randn(size=(100000,128)).to('cuda')
 index = torch.randint(high=src.size(0), size=(100000,)).to('cuda')
