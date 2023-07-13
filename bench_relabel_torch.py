4220db4de15ee7f8aa77082d5b2f8beffdb7232b
Rishi Puri <puririshi98@berkeley.edu>
2023-07-13 13:31:24 -0700

    Update bench_relabel_torch.py

--- bench_relabel_torch.py
+++ bench_relabel_torch.py
@@ -1,7 +1,7 @@
 import torch_geometric
 import torch
 # scale up to see when it breaks
-for i in range(2, 10):
+for i in range(2, 20):
   print("trying w/ num nodes = 10^" + str(i))
   max_index = 10**i
   node_idx = torch.zeros(max_index, dtype=torch.long,
