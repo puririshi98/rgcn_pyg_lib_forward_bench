67f02bf4d5f56fd0fdaf6b86d85848ffea1f54d2
Rishi Puri <puririshi98@berkeley.edu>
2023-07-13 13:30:41 -0700

    Update bench_relabel_torch.py

--- bench_relabel_torch.py
+++ bench_relabel_torch.py
@@ -1,13 +1,8 @@
 import torch_geometric
 import torch
 # scale up to see when it breaks
-subset = torch.tensor([3, 4, 5], device='cuda', dtype=torch.long)
-# scale up to see when it breaks
 for i in range(2, 10):
   print("trying w/ num nodes = 10^" + str(i))
-  num_nodes = 10**i
-  data = torch_geometric.datasets.FakeDataset(avg_num_nodes=num_nodes).data
-  node_idx = torch.zeros(data.x.size(0), dtype=torch.long,
+  max_index = 10**i
+  node_idx = torch.zeros(max_index, dtype=torch.long,
                        device='cuda')
-  node_idx[subset] = torch.arange(3, device='cuda')
-  data.edge_index = node_idx[data.edge_index]
