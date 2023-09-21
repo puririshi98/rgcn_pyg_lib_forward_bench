c31add9ea7545cdb2322cfb8792194e7556a7557
Rishi Puri <puririshi98@berkeley.edu>
2023-09-21 09:25:50 -0700

    Update multinode-papers-100m.py

--- multinode-papers-100m.py
+++ multinode-papers-100m.py
@@ -42,7 +42,7 @@ def create_local_process_group(num_workers_per_node):
     assert _LOCAL_PROCESS_GROUP is None
     world_size = dist.get_world_size() if dist.is_initialized() else 1
     rank = dist.get_rank() if dist.is_initialized() else 0
-    assert world_size % num_workers_per_node == 0
+    assert world_size % num_workers_per_node == 0, "world_size=" + str(world_size)
 
     num_nodes = world_size // num_workers_per_node
     node_rank = rank // num_workers_per_node
