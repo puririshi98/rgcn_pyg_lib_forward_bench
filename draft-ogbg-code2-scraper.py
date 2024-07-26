8c0468d258a5920209f00810906e3efcce2a1e09
Rishi Puri <puririshi98@berkeley.edu>
2024-07-26 10:55:14 -0700

    Update draft-ogbg-code2-scraper.py

--- draft-ogbg-code2-scraper.py
+++ draft-ogbg-code2-scraper.py
@@ -12,29 +12,30 @@ def get_raw_python(func_name_tokens):
 	max_hits = 0
 	max_hit_data_pt = None
 	for raw_data_pt in raw_dataset:
-		func_name = raw_data_pt["func_name"].split('.')
-		if func_name == ''.join(func_name_tokens):
-			return raw_data_pt["whole_func_string"]
+		func_name = raw_data_pt["func_name"].split('.')[-1]
+		if func_name == ''.join(func_name_tokens) or func_name == '_'.join(func_name_tokens):
+			func_str = raw_data_pt["whole_func_string"]
+			# start at the comments, since including the def string is cheating
+			return func_str[func_str.find('"""'):]
 	# if we can't find the function just return empty string
-	return ""
+	raise Error("nothing found for func_name_tokens =", func_name_tokens)
 
 new_set = []
 for i in range(len(dataset)):
 	old_obj = dataset[i]
+	print("Iter", i)
+	print("old_obj =", old_obj)
 	new_obj = Data()
 	# combine all node information into a single feature tensor, let the GNN+LLM figure it out
 	new_obj.x = torch.cat((old_obj.x, old_obj.node_is_attributed, old_obj.node_dfs_order, old_obj.node_depth), dim=1)
 	# extract raw python function for use by LLM 
 	func_name_tokens = old_obj.y
 	new_obj.desc = get_raw_python(func_name_tokens)
-	if new_obj.desc == "":
-		# skip data points with no raw data to match
-		continue
 	# extract other data needed for GNN+LLM
 	new_obj.y = old_obj.y
 	new_obj.edge_index = old_obj.edge_index
 	new_obj.num_nodes = old_obj.num_nodes
 	new_set.append(new_obj)
-	print(new_obj)
+	print("new_obj =", new_obj)
 	del old_obj
 	
