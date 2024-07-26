2fee0144d66a83aea17f977d5d85ea18f0d60e6f
Rishi Puri <puririshi98@berkeley.edu>
2024-07-25 17:39:20 -0700

    Update draft-ogbg-code2-scraper.py

--- draft-ogbg-code2-scraper.py
+++ draft-ogbg-code2-scraper.py
@@ -3,12 +3,12 @@ from torch_geometric.data import Data
 import torch
 import datasets
 dataset = PygGraphPropPredDataset(name="ogbg-code2")
-raw_dataset = datasets.load_dataset("claudios/code_search_net")
+raw_dataset = datasets.load_dataset("claudios/code_search_net", "python")
 raw_dataset = datasets.concatenate_datasets([raw_dataset["train"], raw_dataset["validation"], raw_dataset["test"]])
 def get_raw_python(func_name_tokens):
 	# the ordering of code_search_net does not match ogbg-code2
 	# have to search for matching python
-	# (TODO) figure most efficient way to get the matching python (currently brute forcing)
+	# (TODO) figure most efficient way to get the matching python (currently brute forcing way too slow)
 	max_hits = 0
 	max_hit_data_pt = None
 	for raw_data_pt in raw_dataset:
@@ -23,7 +23,7 @@ for i in range(len(dataset)):
 	old_obj = dataset[i]
 	new_obj = Data()
 	# combine all node information into a single feature tensor, let the GNN+LLM figure it out
-	new_obj.x = torch.cat((old_obj.x, old_obj.node_is_attributed, old_obj.node_dfs_order, old_obj.node_depth))
+	new_obj.x = torch.cat((old_obj.x, old_obj.node_is_attributed, old_obj.node_dfs_order, old_obj.node_depth), dim=1)
 	# extract raw python function for use by LLM 
 	func_name_tokens = old_obj.y
 	new_obj.desc = get_raw_python(func_name_tokens)
@@ -35,5 +35,6 @@ for i in range(len(dataset)):
 	new_obj.edge_index = old_obj.edge_index
 	new_obj.num_nodes = old_obj.num_nodes
 	new_set.append(new_obj)
+	print(new_obj)
 	del old_obj
 	
