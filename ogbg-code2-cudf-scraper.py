c20b89da5b335b0f16c67b07efc6bebe7bfc52f2
Rishi Puri <puririshi98@berkeley.edu>
2024-09-03 15:59:28 -0700

    Update ogbg-code2-cudf-scraper.py

--- ogbg-code2-cudf-scraper.py
+++ ogbg-code2-cudf-scraper.py
@@ -1,4 +1,5 @@
-# run w/ python -m cudf.pandas x.py for GPU acceleration
+# run w/ python -m cudf.pandas x.py for CUDF GPU acceleration
+# 
 from ogb.graphproppred import PygGraphPropPredDataset
 from torch_geometric.data import Data
 import torch
@@ -14,10 +15,34 @@ def make_raw_data_frame():
 	# Create a Data Frame with
 	# column 1: "func_name"
 	# column 2: "whole_func_string"
-	df = pd.DataFrame({'func_name':raw_dataset["func_name"], "whole_func_string":raw_dataset["whole_func_string"]})
+	filtered_func_names = []
+	for i in raw_dataset["func_name"]:
+		split_name = i.split('.')
+		##
+		func_name_tokens = ["new", "datetime", "index"]
+		all_in = all([bool(token in func_name) for token in func_name_tokens])
+		last_pos = func_name.find(func_name_tokens[0])
+		all_in_order = True
+		for token in func_name_tokens[1:]:
+			cur_pos = func_name.find(token)
+			all_in_order = last_pos < cur_pos
+			if not all_in_order:
+				break
+			lost_pos = cur_pos
+		matches = (all_in & all_in_order)
+		##
+		if matches:
+			print("i")
+		if len(split_name) > 1:
+			filtered_func_names.append(split_name[-1].lower())
+		else:
+			filtered_func_names.append(i.lower())
+	df = pd.DataFrame({'func_name':filtered_func_names, "whole_func_string":raw_dataset["whole_func_string"]})
+	df.set_index('func_name', inplace=True)
 	return df
 df = make_raw_data_frame()
 print(df)
+quit()
 def get_raw_python_from_df(func_name_tokens):
 	# the ordering of code_search_net does not match ogbg-code2
 	# have to search for matching python
@@ -26,6 +51,7 @@ def get_raw_python_from_df(func_name_tokens):
 	basic_matches = (func_name == ''.join(func_name_tokens).lower()) | (func_name == '_'.join(func_name_tokens).lower()) | (func_name == "_" + "_".join(func_name_tokens).lower())
 	basic_matches = basic_matches | (func_name == "_" + ''.join(func_name_tokens).lower())
 	basic_matches = basic_matches | (func_name == "_" + ''.join(func_name_tokens).lower() + "_")
+	basic_matches = basic_matches | (func_name == "_" + '_'.join(func_name_tokens).lower() + "_")
 	basic_matches = basic_matches | (func_name == "_" + '_'.join(func_name_tokens).lower() + "_")
 	# all_in = all([bool(token in func_name) for token in func_name_tokens])
 	# last_pos = func_name.find(func_name_tokens[0])
@@ -39,15 +65,15 @@ def get_raw_python_from_df(func_name_tokens):
 	# matches = (basic_matches) | (all_in & all_in_order)
 	matches = basic_matches
 	result = df[matches]
-	print(result)
 	if len(result) > 0:
 		"""
   		Randomly select one of the matches. We randomly select since we
     		have no idea how to break these ties. If this proves to be a blocker for SOTA results,
       		will come back to figure out how to do this best."""
-		selected_result = result[random.randint(0,len(result)-1]
-		func_str = raw_data_pt["whole_func_string"]
-		return func_str[4:(func_str.find(":"))], func_str[func_str.find('"""'):]
+		selected_result = result.sample()
+		func_str = str(selected_result.iloc[0]["whole_func_string"])
+		#print(func_str)
+		return func_str[func_str.find("def"):(func_str.find(":"))], func_str[func_str.find('"""'):]
 	raise ValueError("nothing found for func_name_tokens =", func_name_tokens)
 
 new_set = []
@@ -66,5 +92,5 @@ for i in tqdm(range(len_set)):
 	new_obj.edge_index = old_obj.edge_index
 	new_obj.num_nodes = old_obj.num_nodes
 	new_set.append(new_obj)
-	print("new_obj =", new_obj)
+	#print("new_obj =", new_obj)
 	del old_obj
