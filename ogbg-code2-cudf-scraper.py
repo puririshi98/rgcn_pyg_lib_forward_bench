7e167458851610b5af3e17013f67299721e3dfb6
Rishi Puri <puririshi98@berkeley.edu>
2024-09-03 15:12:47 -0700

    Update ogbg-code2-cudf-scraper.py

--- ogbg-code2-cudf-scraper.py
+++ ogbg-code2-cudf-scraper.py
@@ -5,16 +5,16 @@ import torch
 import datasets
 from tqdm import tqdm
 import pandas as pd
+import random
 ogbg_dataset = PygGraphPropPredDataset(name="ogbg-code2")
 raw_dataset = datasets.load_dataset("claudios/code_search_net", "python")
 raw_dataset = datasets.concatenate_datasets([raw_dataset["train"], raw_dataset["validation"], raw_dataset["test"]])
 
 def make_raw_data_frame():
 	# Create a Data Frame with
-	# index: "func_name"
-	# column: "whole_func_string"
+	# column 1: "func_name"
+	# column 2: "whole_func_string"
 	df = pd.DataFrame({'func_name':raw_dataset["func_name"], "whole_func_string":raw_dataset["whole_func_string"]})
-	df.set_index('func_name', inplace=True)
 	return df
 df = make_raw_data_frame()
 print(df)
@@ -27,19 +27,25 @@ def get_raw_python_from_df(func_name_tokens):
 	basic_matches = basic_matches | (func_name == "_" + ''.join(func_name_tokens).lower())
 	basic_matches = basic_matches | (func_name == "_" + ''.join(func_name_tokens).lower() + "_")
 	basic_matches = basic_matches | (func_name == "_" + '_'.join(func_name_tokens).lower() + "_")
-	all_in = all([bool(token in func_name) for token in func_name_tokens])
-	last_pos = func_name.find(func_name_tokens[0])
-	all_in_order = True
-	for token in func_name_tokens[1:]:
-		cur_pos = func_name.find(token)
-		all_in_order = last_pos < cur_pos
-		if not all_in_order:
-			break
-		lost_pos = cur_pos
-	matches = (basic_matches) | (all_in & all_in_order)
+	# all_in = all([bool(token in func_name) for token in func_name_tokens])
+	# last_pos = func_name.find(func_name_tokens[0])
+	# all_in_order = True
+	# for token in func_name_tokens[1:]:
+	# 	cur_pos = func_name.find(token)
+	# 	all_in_order = last_pos < cur_pos
+	# 	if not all_in_order:
+	# 		break
+	# 	lost_pos = cur_pos
+	# matches = (basic_matches) | (all_in & all_in_order)
+	matches = basic_matches
 	result = df[matches]
 	print(result)
-	if matches:
+	if len(result) > 0:
+		"""
+  		Randomly select one of the matches. We randomly select since we
+    		have no idea how to break these ties. If this proves to be a blocker for SOTA results,
+      		will come back to figure out how to do this best."""
+		selected_result = result[random.randint(0,len(result)-1]
 		func_str = raw_data_pt["whole_func_string"]
 		return func_str[4:(func_str.find(":"))], func_str[func_str.find('"""'):]
 	raise ValueError("nothing found for func_name_tokens =", func_name_tokens)
